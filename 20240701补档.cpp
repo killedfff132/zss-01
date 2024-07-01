31f82035206f2475f86be0a75cbb7cf7c3839b58
killedfff132 <killedfff132@qq.com>
2024-07-01 18:46:57 +0800

    Add files via upload

---
+++ 20240701补档.cpp
@@ -0,0 +1,55 @@
+//#include<stdio.h>
+//void main()
+//{
+//	printf("hello world");
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	a = 100;
+//	a = a - 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	int b;
+//	int c;
+//	a = 10;
+//	b = ++a + 1;
+//	c = a-- - 1;
+//	printf("a=%d\n""b=%d\n""c=%d",  a, b, c);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 5 == 5;
+//	int b = 10 > 10;
+//	printf("a=%d\n""b=%d", a, b);						
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()												关系赋值，真式值1，假式值0
+//{
+//	int a = 5;
+//	int b = 10;
+//	int c = ++a >= 6;
+//	int d = a - 1 >= b;
+//	printf("a=%d\n""b=%d\n""c=%d\n""d=%d", a, b, c, d); return 0;
+//}
+
+
